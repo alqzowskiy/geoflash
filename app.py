18c25a97af61fde087567ce8973854113c5694ad
adlenxk <alazimvak@gmail.com>
2025-04-24 23:49:58 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    with open('static/data.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
